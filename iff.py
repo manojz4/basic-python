522177b652866600ca2c8d751fc7daac3ad7369c
Manz <164121066+manojz4@users.noreply.github.com>
2024-03-22 12:25:06 +0530

    Add files via upload

---
+++ iff.py
@@ -0,0 +1,9 @@
+num =int(input("enter the number"))
+if num %5==0 and num % 3 ==0 :
+     print("Fizz Buzz")
+elif num%3 == 0 :
+     print("Fizz")
+elif num%5 == 0 :
+     print("Buzz")
+else:
+     print("Invalid Input")
